9b477f9f756fdfcbabf737613c01a210d99c8977
Asjad <92135855+AsjadOooO@users.noreply.github.com>
2022-01-11 14:42:23 +0500

    Add files via upload

--- zero-tool.py
+++ zero-tool.py
@@ -2,7 +2,6 @@ import sys,os
 from colorama import Fore
 
 print(Fore.MAGENTA+"""
-
  _______  _______  _______  _______         _______ __________________ _______  _______  _        _______  _______ 
 / ___   )(  ____ \(  ____ )(  ___  )       (  ___  )\__   __/\__   __/(  ___  )(  ____ \| \    /\(  ____ \(  ____ )
 \/   )  || (    \/| (    )|| (   ) |       | (   ) |   ) (      ) (   | (   ) || (    \/|  \  / /| (    \/| (    )|
@@ -13,13 +12,11 @@ print(Fore.MAGENTA+"""
 (_______/(_______/|/   \__/(_______)       |/     \|   )_(      )_(   |/     \|(_______/|_/    \/(_______/|/   \__/
                                                                                                                    
                                                                  
-
 BY: DEV7KNIGHT X ASJAD
 Version 0.1 (0.2 version on its way (more tool)?)
 """)
 
 print(Fore.YELLOW+"""
-
 1: Zero-Tool (Hacking Tools)   | 2:Zero-Web-Hacking-Tool
 3: Zero-Token-Generator        | 4.Spammer (Beta)
 5: Information about us        | 6.SELFBOT(NOT A HACKING TOOL)
